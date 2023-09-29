5123483981194b73a1e65ef5ad41c24969078482
unknown <brillianesa@yahoo.co.id>
2023-09-29 21:04:43 +0700

    delete import

--- src/main/java/com/example/batch25/controller/RegionController.java
+++ src/main/java/com/example/batch25/controller/RegionController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.batch25.dto.RegisterRequest;
 import com.example.batch25.model.Region;
 import com.example.batch25.repository.RegionRepository;
 
