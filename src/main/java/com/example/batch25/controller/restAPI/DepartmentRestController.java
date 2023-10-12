420293fdf57bd7495d216a8950c66b8b4a05722f
Mas Brillianesa Faydhurrahman <brillianesa@yahoo.co.id>
2023-10-12 16:45:04 +0700

    Ajax Jquery

--- src/main/java/com/example/batch25/controller/restAPI/DepartmentRestController.java
+++ src/main/java/com/example/batch25/controller/restAPI/DepartmentRestController.java
@@ -1,6 +1,5 @@
 package com.example.batch25.controller.restAPI;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,45 +12,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.batch25.handler.CustomResponse;
+import com.example.batch25.model.Department;
 import com.example.batch25.model.Region;
-import com.example.batch25.repository.RegionRepository;
+import com.example.batch25.repository.DepartmentRepository;
 
 @RestController
 @RequestMapping("api")
-public class RegionRestController {
+public class DepartmentRestController {
     @Autowired
-    private RegionRepository regionRepository;
+    private DepartmentRepository departmentRepository;
 
-
-    @GetMapping("region")
+    @GetMapping("department")
     public ResponseEntity<Object> Get(){
-        return CustomResponse.generate(HttpStatus.OK, "Data retrieved", regionRepository.findAll());
+        return CustomResponse.generate(HttpStatus.OK, "Data retrieved", departmentRepository.findAll());
     }
 
-    @PostMapping(value = {"region", "region/{id}"})
-    public ResponseEntity<Object> save(@RequestBody Region region, @PathVariable(required = false) Integer id){
+    @PostMapping(value = {"department", "department/{id}"})
+    public ResponseEntity<Object> save(@RequestBody Department department, @PathVariable(required = false) Integer id){
         if (id != null){
-            Region newRegion  = regionRepository.findById(id).orElse(null);
-            newRegion.setId(id);
-            newRegion.setName(region.getName());
-            regionRepository.save(newRegion);
-            Boolean isUpdated = regionRepository.CountByName(newRegion.getName()) > 0 && regionRepository.findById(newRegion.getId()).isPresent();
-            if(isUpdated){
-                return CustomResponse.generate(HttpStatus.OK, "Data saved");
-            }
+            Department newDepartment  = departmentRepository.findById(id).orElse(null);
+            newDepartment.setId(id);
+            newDepartment.setName(department.getName());
+            newDepartment.setRegion(department.getRegion());
+            newDepartment.setDivision(department.getDivision());
+            departmentRepository.save(newDepartment);
+            return CustomResponse.generate(HttpStatus.OK, "Data saved");
         }else{
-            regionRepository.save(region);
-            Boolean isCreated = regionRepository.findById(region.getId()).isPresent();
+            departmentRepository.save(department);
+            Boolean isCreated = departmentRepository.findById(department.getId()).isPresent();
             if(isCreated){
                 return CustomResponse.generate(HttpStatus.OK, "Data saved");
             } 
         }return CustomResponse.generate(HttpStatus.OK, "Data failed to save");
     }
 
-    @DeleteMapping("region/{id}")
+    @DeleteMapping("department/{id}")
     public ResponseEntity<Object> delete(@PathVariable(required = true) Integer id){
-        regionRepository.deleteById(id);
-        Boolean isDeleted = regionRepository.findById(id).isEmpty();
+        departmentRepository.deleteById(id);
+        Boolean isDeleted = departmentRepository.findById(id).isEmpty();
         if(isDeleted){
             return CustomResponse.generate(HttpStatus.OK, "Data deleted");
         }else{
@@ -60,18 +58,14 @@ public class RegionRestController {
         
     }
 
-    @GetMapping("region/{id}")
+    @GetMapping("department/{id}")
     public ResponseEntity<Object> Get(@PathVariable(required = true) Integer id){
-        regionRepository.findById(id);
-        Boolean isExist = regionRepository.findById(id).isPresent();
+        departmentRepository.findById(id);
+        Boolean isExist = departmentRepository.findById(id).isPresent();
         if(isExist){
-            return CustomResponse.generate(HttpStatus.OK, "Data retrieved", regionRepository.findById(id));
+            return CustomResponse.generate(HttpStatus.OK, "Data retrieved", departmentRepository.findById(id));
         }else{
             return CustomResponse.generate(HttpStatus.BAD_REQUEST, "Data failed to retrieve");
         }
     }
-
-    
-
-
 }
